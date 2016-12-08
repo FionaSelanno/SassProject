c0d4dde24bf91e39aebd44be5864d273c0c4fc0d
Fiona Selanno <Fiona@MacBook-Pro-van-Fiona.local>
2016-12-08 16:59:57 +0100

    refactor animation code to reuse it

--- src/js/script.js
+++ src/js/script.js
@@ -7,18 +7,19 @@ $(document).ready(function(){
     var window_top_position = $window.scrollTop();
     var window_bottom_position = (window_top_position + window_height);
 
-    var element_height = $animationElement.outerHeight();
-    var element_top_position = $animationElement.offset().top;
-    var element_bottom_position = (element_top_position + element_height);
+    $.each($animationElement, function(){
+      var $element = $(this);
+      var element_height = $element.outerHeight();
+      var element_top_position = $element.offset().top;
+      var element_bottom_position = (element_top_position + element_height);
 
-    if ((element_bottom_position >= window_top_position) &&
-        (element_top_position <= window_bottom_position)){
-          $animationElement.addClass('animation');
-          $animationElement.addClass('animation');
-        } else {
-          $animationElement.removeClass('animation');
-          $animationElement.removeClass('animation');
-        }
+      if ((element_bottom_position >= window_top_position) &&
+          (element_top_position <= window_bottom_position)){
+            $element.addClass('animation');
+          } else {
+            $element.removeClass('animation');
+          }
+    });
   };
   $window.on('scroll resize', check_if_in_view); //event handler that listens to the scroll and resize events.
   $window.trigger('scroll'); //trigger the scroll event as soon the DOM is ready
